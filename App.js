d60d17df9ae4c6473cb171561144e36b8d43753e
kumarmudur <kumarmudur10@gmail.com>
2026-04-21 20:23:21 +0530

    app changes

--- App.js
+++ App.js
@@ -1,8 +1,49 @@
-const heading = React.createElement(
-        "h1",
-        { id: 'heading'},
-        'Hello World from React AppJS!'
-    );
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import ReactDOM from "react-dom/client";
 
-root.render(heading);
+const Header = () =>{
+  return (
+      <div className="header">
+         <div className="logo-container">
+            <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+         </div>
+          <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+          </div>
+      </div>
+  )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <h1>Meghana Foods</h1>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container"></div>
+        </div>
+    )
+}
+
+const AppLayout =() => {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
