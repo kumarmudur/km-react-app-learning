00ce06da880135186a5a92361dcd0dadb453f4a7
kumarmudur <kumarmudur10@gmail.com>
2026-08-15 21:37:03 -0500

    react components

--- App.js
+++ App.js
@@ -19,10 +19,14 @@ const Header = () =>{
   )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = ({ resName, cuisine }) => {
     return (
-        <div className="res-card">
-            <h1>Meghana Foods</h1>
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0"}}>
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/slheenytwb3iaypqf4yd" />
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 minutes</h4>
         </div>
     )
 }
@@ -31,7 +35,13 @@ const Body = () => {
     return (
         <div className="body">
             <div className="search">Search</div>
-            <div className="res-container"></div>
+            <div className="res-container">
+                <RestaurantCard
+                    resName="Meghana Foods"
+                    cuisine="Biriyani, Andra"
+                />
+                <RestaurantCard resName="KFC"  cuisine="Burger, Fast Food"/>
+            </div>
         </div>
     )
 }
@@ -40,6 +50,7 @@ const AppLayout =() => {
     return (
         <div className="App">
             <Header />
+            <Body />
         </div>
     )
 }
